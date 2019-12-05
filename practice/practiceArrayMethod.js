d2c1229b96ed8b6b46f7ea0c4b2f890cee88ea9f
baekCode <sosaer88@gmail.com>
2019-12-05 23:50:28 +0900

    add, practice

---
+++ practice/practiceArrayMethod.js
@@ -0,0 +1,43 @@
+/**
+ * 반복 연습용 
+ * 배열 메소드 연습용
+ */
+let arr = [1,2,3,4,5]
+let arr1 = [1,2,3,4,5]
+let arr2 = ['홍길동','김철수','김나미']
+let str1 = 'Hello'
+let str2 = 'world'
+
+//기존 데이터가 변하지 않는 배열 concat,join,split,slice
+let arrConcat = ;
+let strConcat = ;
+console.log(arrConcat)
+console.log(strConcat)
+
+let arrJoin = ;
+let strJoin = ;
+console.log(arrJoin)
+console.log(strJoin)
+
+let strSplit = ;
+console.log(strSplit)
+
+let arrSlice = ;
+let strSlice = ;
+console.log(arrSlice)
+console.log(strSlice)
+
+//기존 데이터가 변하는 배열 pop,push,unshift,shift,splice
+let arrPop = ;
+console.log(arrPop)
+
+let arrPush = ;
+console.log(arrPush)
+
+let arrUnshift = ;
+console.log(arrUnshift)
+
+let arrShift = ;
+console.log(arrShift)
+
+//기타 indexOf
